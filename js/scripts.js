af11a2b6f2b12abb22df064d47e4cd7aa6e7fce6
Michelle A <michelleameral@gmail.com>
2022-03-21 08:32:40 -0700

    Possible navbar-in progress

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,10 @@ $(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() == 0) {
             $('#sticky-nav').removeClass('d-background');
+            $('#sticky-nav').addClass('t-background');
         } else {
             $('#sticky-nav').addClass('d-background');
+            $('#sticky-nav').removeClass('t-background');
         }
     });
 });
